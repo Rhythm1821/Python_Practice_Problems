3d60f7efdcdf2e94c10f2958aab812777d35dd78
Rhythm1821 <alamrawat1821@gmail.com>
2023-06-30 14:51:08 +0530

    Ans-1

---
+++ Ans_1.py
@@ -0,0 +1,7 @@
+# 1. Write a Python program to reverse a string without using any built-in string reversal functions.
+def reverse(string):
+    return string[::-1]
+
+a = reverse('MALAYALAM')
+
+print(a)
