a639c617aa70c166d7850f7cc5289992a82493d5
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-06 16:48:04 +0530

    Ans-30

---
+++ Ans_30.py
@@ -0,0 +1,9 @@
+# 30. Implement a function to find the minimum element in a rotated sorted list.
+
+def find_minimum(nums):
+    return min(nums)
+
+# Example usage:
+rotated_list = [4, 5, 6, 7, 1, 2, 3]
+result = find_minimum(rotated_list)
+print(result)
