6a31fa8c9f3582b840a02e595ac48e9db79ba34e
Rhythm1821 <alamrawat1821@gmail.com>
2023-07-06 13:30:47 +0530

    Ans-46

---
+++ Ans_46.py
@@ -0,0 +1,25 @@
+# 46. Implement a function to find the first missing positive
+
+
+def smallest_positive_int(lst:list):
+    pos_num_set = set()
+    for i in lst:
+        if i>0:
+            pos_num_set.add(i)
+
+    if not pos_num_set:
+        return 1
+    
+    start = min(pos_num_set)
+    end = max(pos_num_set)+1
+    
+    for i in range(start,end):
+        if i not in pos_num_set:
+            return i
+    return end
+
+l1 = [0, -10, 1, 2, 3, 4, 5]
+
+
+func = smallest_positive_int(l1)
+print(func)
